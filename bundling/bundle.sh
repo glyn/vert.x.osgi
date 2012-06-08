774df0e9d3d5e62f1dc3ec7f17b467d972efc235
Glyn Normington <glyn.normington@gmail.com>
2012-06-08 15:13:53 +0100

    improve bundle names

---
+++ bundling/bundle.sh
@@ -0,0 +1,3 @@
+bundlor-1.1.0.M04/bin/bundlor.sh -i ./vert.x-core.jar -m ./vertx.core.mf -o ./vert.x-core-1.0.0.final.jar
+bundlor-1.1.0.M04/bin/bundlor.sh -i ./vert.x-platform.jar -m ./vertx.platform.mf -o ./vert.x-platform-1.0.0.final.jar
+
